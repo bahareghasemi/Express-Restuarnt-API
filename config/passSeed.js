6df367f11f8be5612464b89c2cd32cc1c5dd8689
bahareghasemi <bahare.ghasemi.phh@gmail.com>
2024-04-12 17:35:06 -0400

    bcryptjs

--- config/passSeed.js
+++ config/passSeed.js
@@ -4,7 +4,8 @@ const fs = require('fs');
 const run = async() => {
     // Generate random salt value between 5 and 10
     const saltRandom = 10;
-    const password =await bcrypt.hash("bahare", saltRandom);
+    const pass = process.env.PASSWORD;
+    const password =await bcrypt.hash(pass, saltRandom);
     console.log(`PASSWORD=${password}\nSALT=${saltRandom}`);
 }
 run().then(console.log).catch(console.error);
